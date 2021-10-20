76e2738df18327636e1b5586f2c8a5a23011146a
devcabsy <b.devcabsy@gmail.com>
2021-10-20 16:50:24 +0400

    Dual A/B Test 9

--- include/configs/ncx1_mk1.h
+++ include/configs/ncx1_mk1.h
@@ -64,6 +64,21 @@
 #define CONFIG_SYS_LOAD_ADDR		0x22000000	/* load address */
 
 
+
+
+#ifdef CONFIG_SD_BOOT
+/* bootstrap + u-boot + env + linux in sd card */
+#define CONFIG_BOOTCOMMAND  \
+			"fatload mmc 0:1 0x21000000 at91-ncx1_mk1.dtb;" \
+			"fatload mmc 0:1 0x22000000 zImage;" \
+			"bootz 0x22000000 - 0x21000000"
+
+#elif defined(CONFIG_NAND_BOOT)
+/* bootstrap + u-boot + env + linux in nandflash */
+#define CONFIG_BOOTCOMMAND	"nand read " \
+				"0x22000000 0x200000 0x600000; " \
+				"nand read 0x21000000 0x180000 0x20000; " \
+				"bootz 0x22000000 - 0x21000000"
 /*Testing Bank A/B */
  #define CONFIG_EXTRA_ENV_SETTINGS \
 		 "console=ttyS0,115200\0"\
@@ -82,21 +97,7 @@
 		 "bank=a\0"\
 		 "kernel_boot=kernel_a\0"\
 		 "itb_boot=itb_a\0"
-
-#ifdef CONFIG_SD_BOOT
-/* bootstrap + u-boot + env + linux in sd card */
-#define CONFIG_BOOTCOMMAND  \
-			"fatload mmc 0:1 0x21000000 at91-ncx1_mk1.dtb;" \
-			"fatload mmc 0:1 0x22000000 zImage;" \
-			"bootz 0x22000000 - 0x21000000"
-
-#elif defined(CONFIG_NAND_BOOT)
-/* bootstrap + u-boot + env + linux in nandflash */
-#define CONFIG_BOOTCOMMAND	"nand read " \
-				"0x22000000 0x200000 0x600000; " \
-				"nand read 0x21000000 0x180000 0x20000; " \
-				"bootz 0x22000000 - 0x21000000"
-
+		 
 #elif defined(CONFIG_QSPI_BOOT)
 /* bootstrap + u-boot + env + linux in SPI NOR flash */
 #define CONFIG_BOOTCOMMAND	"sf probe 0; "					\
